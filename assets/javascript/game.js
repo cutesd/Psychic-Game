2755faece311198064e10963ec119962b3182e48
Kerri Coupar <kimberly@kimberlycooper.me>
2018-06-11 12:07:04 -0700

    First working version

---
+++ assets/javascript/game.js
@@ -0,0 +1,75 @@
+
+//
+var psychicGame = function (options) {
+
+    var winCnt = 0;
+    var loseCnt = 0;
+    var remain = 0;
+    var guesses = "UIN";
+    var answer = "";
+
+    var winOut = document.getElementById('winOut');
+    var loseOut = document.getElementById('lossOut');
+    var remainOut = document.getElementById('remainOut');
+    var guessOut = document.getElementById('guessOut');
+
+    this.play = function () {
+        startGame();
+
+    }
+
+    this.guess = function (ltr) {
+        if (ltr === answer) {
+            winGame();
+        } else {
+            wrongAnswer(ltr);
+        }
+    }
+
+    function startGame() {
+        remain = options.NumGuesses;
+        guesses = "";
+        var abc = "abcdefghiklmnopqrstuvwxyz";
+        var rnum = Math.floor(Math.random() * abc.length);
+        answer = abc.substr(rnum, 1);
+        console.log(answer);
+        display();
+    }
+
+    function display() {
+        winOut.textContent = winCnt;
+        loseOut.textContent = loseCnt;
+        remainOut.textContent = remain;
+        guessOut.textContent = guesses;
+    }
+
+    function winGame(){
+        alert("YOU WIN!!! \n The letter was: " + answer.toUpperCase());
+        winCnt++;
+        startGame();
+    }
+
+    function wrongAnswer(ltr){
+        remain--;
+        if(remain > 0){
+            guesses += ltr.toUpperCase();
+            display();
+        } else{
+            alert("Sorry, you lost.  Please try again.");
+            loseCnt++;
+            startGame();
+        }
+    }
+
+}
+
+var myGame = new psychicGame({
+    NumGuesses: 10
+});
+myGame.play();
+
+document.addEventListener('keypress', (event) => {
+    const keyName = event.key;
+    myGame.guess(keyName);
+
+});
